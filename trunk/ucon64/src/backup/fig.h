f7c2c7d77e0049727f81cc819dcd6077be0a3a7b
noisyb <noisyb@ddfabd57-4c8b-41e2-9536-ec85e047d1b2>
2001-08-21 09:23:43 +0000

    This commit was generated by cvs2svn to compensate for changes in r22, which included commits to RCS files with non-trunk default branches.

---
+++ trunk/ucon64/src/backup/fig.h
@@ -0,0 +1,5 @@
+#include "../ucon64.h"
+
+#define fig_TITLE "Super Pro Fighter (Q)/Pro Fighter X (Turbo 2)/Double Pro Fighter (X Turbo)/FIG\n1993/1994/19XX China Coach Limited/CCL http://www.ccltw.com.tw"
+
+int fig_usage(int argc,char *argv[]);

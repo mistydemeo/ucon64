f1430b4884d2f20ad21cba192b477356a304e2ff
noisyb <noisyb@ddfabd57-4c8b-41e2-9536-ec85e047d1b2>
2001-08-22 00:03:26 +0000

    *** empty log message ***

---
+++ src/backup/mgd.h
@@ -0,0 +1,5 @@
+#include "../ucon64.h"
+
+#define mgd_TITLE "Multi Game Doctor(2)/Multi Game Hunter/MGH/RAW"
+
+int mgd_usage(int argc,char *argv[]);
---
+++ src/backup/smd.h
@@ -0,0 +1,5 @@
+#include "../ucon64.h"
+
+#define smd_TITLE "Super Magicom Drive/SMD"
+
+int smd_usage(int argc,char *argv[]);

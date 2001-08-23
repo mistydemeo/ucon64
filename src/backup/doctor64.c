6734089b7908461d3b3633c6a443b58445716498
noisyb <noisyb@ddfabd57-4c8b-41e2-9536-ec85e047d1b2>
2001-08-23 14:15:33 +0000

    *** empty log message ***

--- src/backup/doctor64.c
+++ src/backup/doctor64.c
@@ -246,7 +246,7 @@ int doctor64_usage(int argc,char *argv[])
 {
 if(argcmp(argc,argv,"-help"))printf("\n%s\n",doctor64_TITLE);
 
-printf("  -xv64		send/receive to/from Doctor V64; $FILE=PORT\n\
+printf("  -xv64		send/receive ROM to/from Doctor V64; $FILE=PORT\n\
 		receives automatically when $ROM does not exist\n\
 ");
 

375802c3e967f0563e81706ab4a9a46c6b417c46
noisyb <noisyb@ddfabd57-4c8b-41e2-9536-ec85e047d1b2>
2001-08-28 21:42:29 +0000

    NoisyB: added all missing GPL headers

--- src/patch/aps.c
+++ src/patch/aps.c
@@ -1,3 +1,23 @@
+/*
+aps.c - APS support for uCON64
+
+written by 1998 Silo / BlackBag
+	   1999 - 2001 NoisyB (noisyb@gmx.net)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
 /* Apply an APS (Advanced Patch System) File for N64 Images
  * (C)1998 Silo / BlackBag
  *

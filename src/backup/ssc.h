eae1986f299db042b9e389091d2ba1a9c3af772f
noisyb <noisyb@ddfabd57-4c8b-41e2-9536-ec85e047d1b2>
2001-10-11 23:51:03 +0000

    *** empty log message ***

---
+++ src/backup/ssc.h
@@ -0,0 +1,26 @@
+/*
+ssc.h - minimal support for Super Smart Card/SSC
+
+written by 1999 - 2001 NoisyB (noisyb@gmx.net)
+
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+#include "../ucon64.h"
+
+#define ssc_TITLE "Super Smart Card/SSC"
+
+#define ssc_HEADER_START 0
+#define ssc_HEADER_LEN 512

9fb26ff89001a9176ad56474530e4dacd1b2bbca
noisyb <noisyb@ddfabd57-4c8b-41e2-9536-ec85e047d1b2>
2001-10-11 23:43:36 +0000

    *** empty log message ***

---
+++ trunk/ucon64/src/backup/yoko.h
@@ -0,0 +1,26 @@
+/*
+yoko.h - minimal support for Yoko backup unit (Atari 2600, etc.)
+
+written by 1999 - 2001 NoisyB (noisyb@gmx.net)
+
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+#include "../ucon64.h"
+
+#define yoko_TITLE "YOKO backup unit"
+
+#define yoko_HEADER_START 0
+#define yoko_HEADER_LEN 0

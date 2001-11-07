d1021e03c617991d18156875e8e6970ed9307d22
dbjh <dbjh@ddfabd57-4c8b-41e2-9536-ec85e047d1b2>
2001-11-07 23:32:58 +0000

    -dbjh:

--- src/backup/smd.c
+++ src/backup/smd.c
@@ -2,7 +2,7 @@
 smd.c - Super Magic Drive support for uCON64
 
 written by 1999 - 2001 NoisyB (noisyb@gmx.net)
-
+                  2001 dbjh
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -18,10 +18,6 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
-#include "smd.h"
-
-int smd_argc;
-char *smd_argv[128];
 
 /*
  smd - A Super Magic Drive utility
@@ -77,61 +73,66 @@ char *smd_argv[128];
  It's been reported that there are some difficulties when using 8MB
  and 24MB copiers. I am working on ways to support these devices properly.
 */
-#ifdef __BEOS__
-#include <OS.h>
 
-#ifndef _T_H_
-#define _T_H_
-/*
-#define DRV_READ_IO_8 'r'
-#define DRV_WRITE_IO_8 'w'
-
-typedef struct IO_Tuple
-{
-	unsigned long Port;
-	unsigned char Data;
-} IO_Tuple;
-
-unsigned char inp(unsigned long port);
-void outp(unsigned long port, unsigned char data);
-int par;
-*/
-#endif
-#define sleep(32) snooze(1)
-#endif
 #include "smd.h"
+#include <stdio.h>
+#include <malloc.h>
+#include <string.h>
+#ifndef __DOS__
+#include <unistd.h>                             // usleep()
+#else
+#include <dos.h>                                // delay()
+#endif
+
+typedef unsigned char uint8;
+typedef unsigned short int uint16;
+typedef unsigned long int uint32;
+
+static int smd_argc;
+static char *smd_argv[128];
+static uint16 lpt;
+static uint8 block[0x4000];
+static char *opts[] = {"-bc", "-lc", "-bs", "-ls", "-ci", "-db", "-rc", NULL};
 
 /* from loader.asm */
-
-unsigned char loader[0x100] =
+static unsigned char loader[0x100] =
 {
-	 0xF3, 0xDB, 0xBF, 0x31, 0xF0, 0xDF, 0x21, 0x00, 0xC0, 0x11, 0x01, 0xC0, 0x01, 0xFF, 0x0F, 0xAF,
-	 0x77, 0xED, 0xB0, 0x21, 0x00, 0xD1, 0x11, 0x01, 0xD1, 0x01, 0xFF, 0x0E, 0xAF, 0x77, 0xED, 0xB0,
-	 0x21, 0xB0, 0xD0, 0x01, 0xBF, 0x16, 0xED, 0xB3, 0xAF, 0xD3, 0xBF, 0x3E, 0xC0, 0xD3, 0xBF, 0xAF,
-	 0x0E, 0x3E, 0xD3, 0xBE, 0x0D, 0x20, 0xFB, 0xAF, 0xD3, 0xBF, 0x3E, 0x40, 0xD3, 0xBF, 0xAF, 0x06,
-	 0x3F, 0x0E, 0x00, 0xD3, 0xBE, 0x0D, 0x20, 0xFB, 0x05, 0x20, 0xF6, 0xAF, 0xD3, 0xBF, 0xD3, 0xBF,
-	 0xDD, 0xE5, 0xDD, 0xE1, 0xDB, 0xBE, 0xDD, 0xE5, 0xDD, 0xE1, 0xAF, 0xD3, 0xBF, 0x3E, 0x40, 0xD3,
-	 0xBF, 0xDB, 0xBF, 0xAF, 0xED, 0x47, 0xED, 0x4F, 0xDD, 0x21, 0xFF, 0xFF, 0xFD, 0x21, 0xFF, 0xFF,
-	 0x01, 0x00, 0x00, 0x11, 0x00, 0x00, 0x21, 0x00, 0x00, 0xE5, 0xF1, 0x08, 0xD9, 0x01, 0x00, 0x00,
-	 0x11, 0x00, 0x00, 0x21, 0x00, 0x00, 0xE5, 0xF1, 0x08, 0xD9, 0x31, 0x00, 0x00, 0xFB, 0xED, 0x46,
-	 0xAF, 0x32, 0x00, 0x20, 0x3E, 0x01, 0x32, 0x01, 0x20, 0x00, 0x00, 0x00, 0x00, 0xAF, 0x32, 0xFC,
-	 0xFF, 0x32, 0xFD, 0xFF, 0x3C, 0x32, 0xFE, 0xFF, 0x3C, 0x32, 0xFF, 0xFF, 0xAF, 0xC3, 0x00, 0x00,
-	 0x00, 0x80, 0x00, 0x81, 0x00, 0x82, 0x00, 0x83, 0x00, 0x84, 0x00, 0x85, 0x00, 0x86, 0x00, 0x87,
-	 0x00, 0x88, 0x00, 0x89, 0x00, 0x8A, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-	 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-	 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-	 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+         0xF3, 0xDB, 0xBF, 0x31, 0xF0, 0xDF, 0x21, 0x00, 0xC0, 0x11, 0x01, 0xC0, 0x01, 0xFF, 0x0F, 0xAF,
+         0x77, 0xED, 0xB0, 0x21, 0x00, 0xD1, 0x11, 0x01, 0xD1, 0x01, 0xFF, 0x0E, 0xAF, 0x77, 0xED, 0xB0,
+         0x21, 0xB0, 0xD0, 0x01, 0xBF, 0x16, 0xED, 0xB3, 0xAF, 0xD3, 0xBF, 0x3E, 0xC0, 0xD3, 0xBF, 0xAF,
+         0x0E, 0x3E, 0xD3, 0xBE, 0x0D, 0x20, 0xFB, 0xAF, 0xD3, 0xBF, 0x3E, 0x40, 0xD3, 0xBF, 0xAF, 0x06,
+         0x3F, 0x0E, 0x00, 0xD3, 0xBE, 0x0D, 0x20, 0xFB, 0x05, 0x20, 0xF6, 0xAF, 0xD3, 0xBF, 0xD3, 0xBF,
+         0xDD, 0xE5, 0xDD, 0xE1, 0xDB, 0xBE, 0xDD, 0xE5, 0xDD, 0xE1, 0xAF, 0xD3, 0xBF, 0x3E, 0x40, 0xD3,
+         0xBF, 0xDB, 0xBF, 0xAF, 0xED, 0x47, 0xED, 0x4F, 0xDD, 0x21, 0xFF, 0xFF, 0xFD, 0x21, 0xFF, 0xFF,
+         0x01, 0x00, 0x00, 0x11, 0x00, 0x00, 0x21, 0x00, 0x00, 0xE5, 0xF1, 0x08, 0xD9, 0x01, 0x00, 0x00,
+         0x11, 0x00, 0x00, 0x21, 0x00, 0x00, 0xE5, 0xF1, 0x08, 0xD9, 0x31, 0x00, 0x00, 0xFB, 0xED, 0x46,
+         0xAF, 0x32, 0x00, 0x20, 0x3E, 0x01, 0x32, 0x01, 0x20, 0x00, 0x00, 0x00, 0x00, 0xAF, 0x32, 0xFC,
+         0xFF, 0x32, 0xFD, 0xFF, 0x3C, 0x32, 0xFE, 0xFF, 0x3C, 0x32, 0xFF, 0xFF, 0xAF, 0xC3, 0x00, 0x00,
+         0x00, 0x80, 0x00, 0x81, 0x00, 0x82, 0x00, 0x83, 0x00, 0x84, 0x00, 0x85, 0x00, 0x86, 0x00, 0x87,
+         0x00, 0x88, 0x00, 0x89, 0x00, 0x8A, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+         0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+         0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+         0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 };
 
-uint16 lpt;// = 0x378;
-uint8 block[0x4000];
-char *opts[] = {"-bc", "-lc", "-bs", "-ls", "-ci", "-db", "-rc", NULL};
+/* Function prototypes */
+static void smd_send_byte(uint8 data);
+static uint8 smd_recieve_byte(void);
+static void smd_send_command(uint8 parameter, uint16 address, uint16 length);
+static uint8 smd_recieve_block(uint32 length, uint8 *buffer);
+static void smd_send_block(uint32 length, uint8 *buffer);
+static void smd_poke(uint16 address, uint8 data);
+static uint8 smd_peek(uint16 address);
+static int save_smd(char *filename);
+static int load_smd(char *filename);
+static void interleave_buffer(uint8 *buffer, int size);
+static int load_sram(char *filename);
+static int save_sram(char *filename);
+static int dump_bios(char *filename);
 
 int smd_main (int argc, char **argv)
 {
     int i;
-
-    
 
     /* Print help text */
 /*
@@ -143,7 +144,7 @@ int smd_main (int argc, char **argv)
         exit(1);
     }
 */
-	/* Print help text */
+        /* Print help text */
 /*
     if(strcmp(argv[1], "-help") == 0)
     {
@@ -167,52 +168,25 @@ int smd_main (int argc, char **argv)
             switch(i)
             {
                 case 0: /* Backup cartridge */
-                	#ifdef __BEOS__
-                	par = open("/dev/misc/parnew", O_NONBLOCK|O_RDWR);
-                	#endif
                     save_smd(argv[2]);
-                    #ifdef __BEOS__
-                    close(par);
-                    #endif
                     break;
 
                 case 1: /* Send cartridge */
-                	#ifdef __BEOS__
-                	par = open("/dev/misc/parnew", O_NONBLOCK|O_RDWR);
-                	#endif
                     load_smd(argv[2]);
-                    #ifdef __BEOS__
-                    close(par);
-                    #endif
                     break;
 
                 case 2: /* Backup SRAM */
-                	#ifdef __BEOS__
-                	par = open("/dev/misc/parnew", O_NONBLOCK|O_RDWR);
-                	#endif
                     save_sram(argv[2]);
-                    #ifdef __BEOS__
-                    close(par);
-                    #endif
                     break;
 
                 case 3: /* Send SRAM */
-                	#ifdef __BEOS__
-                	par = open("/dev/misc/parnew", O_NONBLOCK|O_RDWR);
-                	#endif
                     load_sram(argv[2]);
-                    #ifdef __BEOS__
-                    close(par);
-                    #endif
                     break;
 
                 case 4: /* Show cartridge information */
                     {
-                    	uint8 mem;
+                        uint8 mem;
                         uint8 blocks;
-                    	#ifdef __BEOS__
-                    	par = open("/dev/misc/parnew", O_NONBLOCK|O_RDWR);
-                    	#endif
                         mem = smd_peek(0xDFF0);
                         blocks = smd_peek(0xDFF1);
 
@@ -222,30 +196,15 @@ int smd_main (int argc, char **argv)
                             printf("The SMD does not detect a cartridge.\n");
                         else
                             printf("The cartidge size is %d bytes. (%dMB)\n", blocks * 0x4000, (blocks * 0x4000) / 0x20000);
-                    	#ifdef __BEOS__
-                    	close(par);
-                    	#endif
                     }
                     break;
 
                 case 5: /* Dump BIOS */
-                	#ifdef __BEOS__
-                	par = open("/dev/misc/parnew", O_NONBLOCK|O_RDWR);
-                	#endif
                     dump_bios(argv[2]);
-                    #ifdef __BEOS__
-                    close(par);
-                    #endif
                     break;
 
                 case 6: /* Run loaded cartridge */
-                	#ifdef __BEOS__
-                	par = open("/dev/misc/parnew", O_NONBLOCK|O_RDWR);
-                	#endif
                     smd_poke(0x2001, 0x02);
-                    #ifdef __BEOS__
-                    close(par);
-                    #endif
                     break;
             }
         }
@@ -261,12 +220,12 @@ int smd_main (int argc, char **argv)
 void smd_wait_busy(int state)
 {
     int timeout = 0x2000;
-    int busy = (inportb(LPT_STATUS) >> 7) & 1;
+    int busy = (inportb(lpt + PARPORT_STATUS) >> 7) & 1;
 
     while((busy == state) && (timeout))
     {
-        busy = (inportb(LPT_STATUS) >> 7) & 1;
-        timeout -= 1;      
+        busy = (inportb(lpt + PARPORT_STATUS) >> 7) & 1;
+        timeout -= 1;
     }
 
     if(!timeout)
@@ -282,10 +241,10 @@ void smd_send_byte(uint8 data)
     smd_wait_busy(0);
 
     /* Send data to SMD */
-    outportb(LPT_OUTPUT, data);
+    outportb(lpt + PARPORT_DATA, data);
 
     /* Invert PC busy flag */
-    outportb(LPT_CONTROL, inportb(LPT_CONTROL) ^ 1);
+    outportb(lpt + PARPORT_CONTROL, inportb(lpt + PARPORT_CONTROL) ^ 1);
 }
 
 uint8 smd_recieve_byte(void)
@@ -296,19 +255,19 @@ uint8 smd_recieve_byte(void)
     smd_wait_busy(1);
 
     /* Read low nibble from SMD */
-    temp = (inportb(LPT_STATUS) & 0x78) >> 3;
+    temp = (inportb(lpt + PARPORT_STATUS) & 0x78) >> 3;
 
     /* Invert PC busy flag */
-    outportb(LPT_CONTROL, inportb(LPT_CONTROL) ^ 1);
+    outportb(lpt + PARPORT_CONTROL, inportb(lpt + PARPORT_CONTROL) ^ 1);
 
     /* Wait for SMD busy flag to be cleared */
     smd_wait_busy(1);
 
     /* Read high nibble from SMD */
-    temp |= (inportb(LPT_STATUS) & 0x78) << 1;
+    temp |= (inportb(lpt + PARPORT_STATUS) & 0x78) << 1;
 
     /* Invert PC busy flag */
-    outportb(LPT_CONTROL, inportb(LPT_CONTROL) ^ 1);
+    outportb(lpt + PARPORT_CONTROL, inportb(lpt + PARPORT_CONTROL) ^ 1);
 
     return (temp);
 }
@@ -352,7 +311,11 @@ uint8 smd_recieve_block(uint32 length, uint8 *buffer)
     uint32 count;
     uint8 temp, checksum = 0x81;
 
-    sleep(32);
+#ifndef __DOS__                                 // wait 32 milliseconds
+    usleep(32000);
+#else
+    delay(32);
+#endif
 
     /* Read data from the SMD */
     for(count = 0; count < length; count += 1)
@@ -471,7 +434,7 @@ int load_smd(char *filename)
             header[1] = 0x03;
             interleave_buffer(buf, block_size);
         }
-        else 
+        else
         {
             /* Assume all other extensions are Z80 programs */
             header[1] = 0x01;
@@ -509,8 +472,8 @@ int load_smd(char *filename)
     }
     else
     {
-        /* Disable M3 and enable program execution out of copier DRAM. 
-           For >2MB games, write 0x07 on SMD+ copiers. 
+        /* Disable M3 and enable program execution out of copier DRAM.
+           For >2MB games, write 0x07 on SMD+ copiers.
            This turns off DRAM refresh on my original 16MB SMD, since
            bit 3 enables battery backed SRAM instead of having a
            special functions */
@@ -682,122 +645,82 @@ int dump_bios(char *filename)
     return (1);
 }
 
-/*
-#ifdef __BEOS__
-uint8 inp(uint32 port)
+int smd_usage(int argc, char *argv[])
 {
-	IO_Tuple temp;
-	
-	temp.Port = port;
+  if(argcmp(argc, argv, "-help"))
+    printf("\n%s\n",smd_TITLE);
 
-	ioctl(par, DRV_READ_IO_8, &temp, 0);
-	
-	return (temp.Data);
-}
+  printf("  -xsmd         send/receive ROM to/from Super Magicom Drive/SMD; $FILE=PORT\n"
+         "                receives automatically when $ROM does not exist\n"
+         "  -xsmds        send/receive SRAM to/from Super Magicom Drive/SMD; $FILE=PORT\n"
+         "                receives automatically when $ROM(=SRAM) does not exist\n");
 
+  if(argcmp(argc, argv, "-help"))
+  {
+  //TODO more info like technical info about cabeling and stuff for the copier
+  }
 
-void outp(uint32 port, uint8 data)
-{
-	IO_Tuple temp;
-	
-	temp.Port = port;
-    temp.Data = data;
-
-	ioctl(par, DRV_WRITE_IO_8, &temp, 0);
-	
-	return;
-}
-
-#endif
-*/
-
-int smd_usage(int argc,char *argv[])
-{
-if(argcmp(argc,argv,"-help"))printf("\n%s\n",smd_TITLE);
-
-
-printf(	"  -xsmd		send/receive ROM to/from Super Magicom Drive/SMD; $FILE=PORT\n"
-	"		receives automatically when $ROM does not exist\n"
-         "  -xsmds	send/receive SRAM to/from Super Magicom Drive/SMD; $FILE=PORT\n"
-         "                receives automatically when $ROM(=SRAM) does not exist\n"
-);
-
-if(argcmp(argc,argv,"-help"))
-{
-//TODO more info like technical info about cabeling and stuff for the copier
-
-}
-	return(0);
+  return 0;
 }
 
 int smd_read_rom(char *filename, unsigned int parport)
 {
-  lpt=parport;
+  lpt = parport;
 
-  smd_argv[0]="ucon64";
-  smd_argv[1]="-bc";
-  smd_argv[2]=filename;
-  smd_argc=3;
+  smd_argv[0] = "ucon64";
+  smd_argv[1] = "-bc";
+  smd_argv[2] = filename;
+  smd_argc = 3;
 
-  smd_main(smd_argc,smd_argv);
+  smd_main(smd_argc, smd_argv);
 
-  return(0);
+  return 0;
 }
 
 int smd_write_rom(char *filename, unsigned int parport)
 {
-  lpt=parport;
+  lpt = parport;
 
-  smd_argv[0]="ucon64";
-  smd_argv[1]="-lc";
-  smd_argv[2]=filename;
-  smd_argc=3;
+  smd_argv[0] = "ucon64";
+  smd_argv[1] = "-lc";
+  smd_argv[2] = filename;
+  smd_argc = 3;
 
-  smd_main(smd_argc,smd_argv);
+  smd_main(smd_argc, smd_argv);
 
-  smd_argv[0]="ucon64";
-  smd_argv[1]="-rc";
-  smd_argc=2;
+  smd_argv[0] = "ucon64";
+  smd_argv[1] = "-rc";
+  smd_argc = 2;
 
-  smd_main(smd_argc,smd_argv);
+  smd_main(smd_argc, smd_argv);
 
-        return(0);
+  return 0;
 }
 
 int smd_read_sram(char *filename, unsigned int parport)
 {
-  lpt=parport;
+  lpt = parport;
 
-  smd_argv[0]="ucon64";
-  smd_argv[1]="-bs";
-  smd_argv[2]=filename;
-  smd_argc=3;
+  smd_argv[0] = "ucon64";
+  smd_argv[1] = "-bs";
+  smd_argv[2] = filename;
+  smd_argc = 3;
 
-  smd_main(smd_argc,smd_argv);
+  smd_main(smd_argc, smd_argv);
 
-        return(0);
+  return 0;
 }
 
 int smd_write_sram(char *filename, unsigned int parport)
 {
-  lpt=parport;
+  lpt = parport;
 
-  smd_argv[0]="ucon64";
-  smd_argv[1]="-ls";
-  smd_argv[2]=filename;
-  smd_argc=3;
+  smd_argv[0] = "ucon64";
+  smd_argv[1] = "-ls";
+  smd_argv[2] = filename;
+  smd_argc = 3;
 
-  smd_main(smd_argc,smd_argv);
+  smd_main(smd_argc, smd_argv);
 
-        return(0);
+  return 0;
 }
-
-/*
-        printf(" -bc <file.smd>\tSave Mega Drive cartridge to disk.\n");
-        printf(" -lc <file.ext>\tLoad Mega Drive (SMD, BIN) or SMS cartridge.\n");
-        printf(" -bs <file.smd>\tSave SRAM data to disk.\n");
-        printf(" -ls <file.smd>\tLoad SRAM data.\n");
-        printf(" -ci           \tShow loaded cartridge size and available SMD RAM.\n");
-        printf(" -db <file.bin>\tDump SMD BIOS to disk.\n");
-        printf(" -rc           \tRun loaded cartridge.\n");
-*/

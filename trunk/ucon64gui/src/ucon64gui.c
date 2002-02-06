bf7033580611353290bcc3889cb1f7baf7d57232
noisyb <noisyb@ddfabd57-4c8b-41e2-9536-ec85e047d1b2>
2002-02-06 15:15:38 +0000

    *** empty log message ***

--- trunk/ucon64gui/src/ucon64gui.c
+++ trunk/ucon64gui/src/ucon64gui.c
@@ -1,3 +1,23 @@
+/*
+ucon64gui.c - a GUI for ucon64 (based on html2gui by Dirk Reinelt)
+
+written by 2002 NoisyB (noisyb@gmx.net)
+           
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
 #include "ucon64gui.h"
 
 struct html2gui_ index_html;
@@ -70,7 +90,7 @@ main (int argc, char *argv[])
 //<html>
   html2gui_html (&snes_html, 640, 400, 0);
 
-  html2gui_title (&snes_html, "ucon64gui", icon_xpm);
+  html2gui_title (&snes_html, "ucon64gui_snes", icon_xpm);
 
   html2gui_button (&snes_html, ucon64_nfo, "NFO", "Click here to see ROM info", 10, 10, icon_xpm);
 
--- trunk/ucon64gui/src/ucon64gui.h
+++ trunk/ucon64gui/src/ucon64gui.h
@@ -1,3 +1,27 @@
+/*
+ucon64gui.c - a GUI for ucon64 (based on html2gui by Dirk Reinelt)
+
+written by 2002 NoisyB (noisyb@gmx.net)
+           
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+#ifndef UCON64GUI_H
+#define UCON64GUI_H
+
 #include "misc.h"
 
 #include "html2gui_misc.h"
+#endif /* UCON64GUI_H */

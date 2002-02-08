17bb7d53daad4c2dcb9954213feb9cf2f72fb166
noisyb <noisyb@ddfabd57-4c8b-41e2-9536-ec85e047d1b2>
2002-02-08 09:37:40 +0000

    *** empty log message ***

---
+++ src/backup/dex.c
@@ -0,0 +1,40 @@
+/*
+dex.c - DexDrive support for uCON64
+
+written by 2002 NoisyB (noisyb@gmx.net)
+
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+#include "dex.h"
+
+int
+dex_usage (int argc, char *argv[])
+{
+  if (argcmp (argc, argv, "-help"))
+    printf ("%s\n", dex_TITLE);
+
+
+  printf
+    ("TODO:  -xdex    send/receive SRAM to/from DexDrive; $FILE=PORT\n"
+     "		receives automatically when $ROM does not exist\n");
+
+  if (argcmp (argc, argv, "-help"))
+    {
+//TODO more info like technical info about cabeling and stuff for the copier
+
+    }
+  return (0);
+}
---
+++ src/backup/dex.h
@@ -0,0 +1,30 @@
+/*
+dex.h - DexDrive support for uCON64
+
+written by 2002 NoisyB (noisyb@gmx.net)
+
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+#ifndef DEX_H
+#define DEX_H
+#include "../ucon64.h"
+
+#define dex_TITLE "DexDrive\nInterAct http://www.dexdrive.de"
+
+int dex_usage (int argc, char *argv[]);
+#define dex_HEADER_START 0
+#define dex_HEADER_LEN 0
+#endif /* DEX_H */

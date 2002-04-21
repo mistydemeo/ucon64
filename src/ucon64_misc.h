2b8bf91aca472338b48bd8dec19e7d7f574985c9
noisyb <noisyb@ddfabd57-4c8b-41e2-9536-ec85e047d1b2>
2002-04-21 12:18:28 +0000

    *** empty log message ***

--- src/ucon64_misc.h
+++ src/ucon64_misc.h
@@ -56,14 +56,12 @@ IO_Tuple;
 #define PARPORT_DATA    0       // output
 #define PARPORT_STATUS  1       // input
 #define PARPORT_CONTROL 2
-
 /*
   defines for unknown backup units/emulators
 */
 #define unknown_HEADER_START 0
 #define unknown_HEADER_LEN 512
 extern char *unknown_title;
-
 
 // GameGenie "codec" routines
 extern char hexDigit (int value);
@@ -97,4 +95,42 @@ extern int ucon64_gauge (time_t init_time, long pos, long size);
 
 extern int ucon64_trackmode_probe (long imagesize);
 
+#define ISODCL(from, to) (to - from + 1)
+
+struct iso_primary_descriptor
+{
+  char type[ISODCL (1, 1)];   /* 711 */
+  char id[ISODCL (2, 6)];
+  char version[ISODCL (7, 7)];        /* 711 */
+  char unused1[ISODCL (8, 8)];
+  char system_id[ISODCL (9, 40)];     /* achars */
+  char volume_id[ISODCL (41, 72)];    /* dchars */
+  char unused2[ISODCL (73, 80)];
+  char volume_space_size[ISODCL (81, 88)];    /* 733 */
+  char unused3[ISODCL (89, 120)];
+  char volume_set_size[ISODCL (121, 124)];    /* 723 */
+  char volume_sequence_number[ISODCL (125, 128)];     /* 723 */
+  char logical_block_size[ISODCL (129, 132)]; /* 723 */
+  char path_table_size[ISODCL (133, 140)];    /* 733 */
+  char type_l_path_table[ISODCL (141, 144)];  /* 731 */
+  char opt_type_l_path_table[ISODCL (145, 148)];      /* 731 */
+  char type_m_path_table[ISODCL (149, 152)];  /* 732 */
+  char opt_type_m_path_table[ISODCL (153, 156)];      /* 732 */
+  char root_directory_record[ISODCL (157, 190)];      /* 9.1 */
+  char volume_set_id[ISODCL (191, 318)];      /* dchars */
+  char publisher_id[ISODCL (319, 446)];       /* achars */
+  char preparer_id[ISODCL (447, 574)];        /* achars */
+  char application_id[ISODCL (575, 702)];     /* achars */
+  char copyright_file_id[ISODCL (703, 739)];  /* 7.5 dchars */
+  char abstract_file_id[ISODCL (740, 776)];   /* 7.5 dchars */
+  char bibliographic_file_id[ISODCL (777, 813)];      /* 7.5 dchars */
+  char creation_date[ISODCL (814, 830)];      /* 8.4.26.1 */
+  char modification_date[ISODCL (831, 847)];  /* 8.4.26.1 */
+  char expiration_date[ISODCL (848, 864)];    /* 8.4.26.1 */
+  char effective_date[ISODCL (865, 881)];     /* 8.4.26.1 */
+  char file_structure_version[ISODCL (882, 882)];     /* 711 */
+  char unused4[ISODCL (883, 883)];
+  char application_data[ISODCL (884, 1395)];
+  char unused5[ISODCL (1396, 2048)];
+};
 #endif // #ifndef UCON64_MISC_H

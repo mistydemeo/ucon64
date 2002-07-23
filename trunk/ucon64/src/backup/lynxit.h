ddfdb4ebd50817b854e2790d0c1319f3f6409aa5
noisyb <noisyb@ddfabd57-4c8b-41e2-9536-ec85e047d1b2>
2002-07-23 14:11:18 +0000

    *** empty log message ***

--- trunk/ucon64/src/backup/lynxit.h
+++ trunk/ucon64/src/backup/lynxit.h
@@ -1,18 +1,31 @@
-void usage(void)
-BOOL ptr_port_init(UWORD port)
-void lynxit_shift_out_byte(UBYTE data)
-UBYTE lynxit_shift_in_byte(void)
-void lynxit_write_control(UBYTE data)
-void lynxit_write_page(UBYTE page)
-void lynxit_counter_reset(void)
-void lynxit_counter_increment(void)
-UBYTE cart_read_byte(UWORD cart)
-void cart_write_byte(UWORD cart,UBYTE data)
-void cart_read_page(UWORD cart,UWORD page_number,UWORD page_size,UBYTE *page_ptr)
-void cart_write_page(UWORD cart,UWORD page_number,UWORD page_size,UBYTE *page_ptr)
-SWORD cart_analyse(cart)
-BOOL cart_verify(UBYTE *filename)
-BOOL cart_read(UBYTE *filename)
-BOOL cart_write(UBYTE *filename)
-BOOL perform_test(UBYTE *testname)
-void main(SWORD argc,UBYTE **argv)
+/*
+lynxit.h - lynxit support for uCON64
+
+written by 2002 NoisyB (noisyb@gmx.net)
+
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+#ifndef LYNXIT_H
+#define LYNXIT_H
+
+extern const char *lynxit_usage[];
+
+#ifdef BACKUP
+#endif // BACKUP
+
+#define LYNXIT_HEADER_START 0
+#define LYNXIT_HEADER_LEN 0
+#endif // LYNXIT_H

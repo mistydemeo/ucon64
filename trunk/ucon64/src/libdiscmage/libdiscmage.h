1faba51ac818767d076ed9d761fed03939d76eac
noisyb <noisyb@ddfabd57-4c8b-41e2-9536-ec85e047d1b2>
2002-11-11 18:54:30 +0000

    *** empty log message ***

---
+++ trunk/ucon64/src/libdiscmage/libdiscmage.h
@@ -0,0 +1,252 @@
+/*
+libdiscmage.h - DISCmage support routines
+
+written by 1999 - 2001 NoisyB (noisyb@gmx.net)
+              ? - 2002 Dext
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+#ifndef LIBDISCMAGE_H
+#define LIBDISCMAGE_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <inttypes.h>
+#include "config.h"
+#include "audio.h"
+#include "buffer.h"
+#include "quick_io.h"
+#include "misc.h"
+#include "sheets.h"
+#include "unzip.h"
+#ifdef  __linux__
+#include <linux/cdrom.h>
+#else
+/*
+  some useful structs and defines from <linux/cdrom.h>
+*/
+
+/*
+ * -- <linux/cdrom.h>
+ * General header file for linux CD-ROM drivers 
+ * Copyright (C) 1992         David Giller, rafetmad@oxy.edu
+ *               1994, 1995   Eberhard Moenkeberg, emoenke@gwdg.de
+ *               1996         David van Leeuwen, david@tm.tno.nl
+ *               1997, 1998   Erik Andersen, andersee@debian.org
+ *               1998-2000    Jens Axboe, axboe@suse.de
+ */
+ 
+/*******************************************************
+ * CDROM IOCTL structures
+ *******************************************************/
+
+/* Address in MSF libdiscmage */
+struct cdrom_msf0		
+{
+	__u8	minute;
+	__u8	second;
+	__u8	frame;
+};
+
+/* Address in either MSF or logical libdiscmage */
+union cdrom_addr		
+{
+	struct cdrom_msf0	msf;
+	int			lba;
+};
+
+/* This struct is used by the CDROMPLAYMSF ioctl */ 
+struct cdrom_msf 
+{
+	__u8	cdmsf_min0;	/* start minute */
+	__u8	cdmsf_sec0;	/* start second */
+	__u8	cdmsf_frame0;	/* start frame */
+	__u8	cdmsf_min1;	/* end minute */
+	__u8	cdmsf_sec1;	/* end second */
+	__u8	cdmsf_frame1;	/* end frame */
+};
+
+
+/*
+ * A CD-ROM physical sector size is 2048, 2052, 2056, 2324, 2332, 2336, 
+ * 2340, or 2352 bytes long.  
+
+*         Sector types of the standard CD-ROM data libdiscmages:
+ *
+ * libdiscmage   sector type               user data size (bytes)
+ * -----------------------------------------------------------------------------
+ *   1     (Red Book)    CD-DA          2352    (CD_FRAMESIZE_RAW)
+ *   2     (Yellow Book) Mode1 Form1    2048    (CD_FRAMESIZE)
+ *   3     (Yellow Book) Mode1 Form2    2336    (CD_FRAMESIZE_RAW0)
+ *   4     (Green Book)  Mode2 Form1    2048    (CD_FRAMESIZE)
+ *   5     (Green Book)  Mode2 Form2    2328    (2324+4 spare bytes)
+ *
+ *
+ *       The layout of the standard CD-ROM data libdiscmages:
+ * -----------------------------------------------------------------------------
+ * - audio (red):                  | audio_sample_bytes |
+ *                                 |        2352        |
+ *
+ * - data (yellow, mode1):         | sync - head - data - EDC - zero - ECC |
+ *                                 |  12  -   4  - 2048 -  4  -   8  - 276 |
+ *
+ * - data (yellow, mode2):         | sync - head - data |
+ *                                 |  12  -   4  - 2336 |
+ *
+ * - XA data (green, mode2 form1): | sync - head - sub - data - EDC - ECC |
+ *                                 |  12  -   4  -  8  - 2048 -  4  - 276 |
+ *
+ * - XA data (green, mode2 form2): | sync - head - sub - data - Spare |
+ *                                 |  12  -   4  -  8  - 2324 -  4    |
+ *
+ */
+
+/* Some generally useful CD-ROM inlibdiscmageion -- mostly based on the above */
+#define CD_MINS              74 /* max. minutes per CD, not really a limit */
+#define CD_SECS              60 /* seconds per minute */
+#define CD_FRAMES            75 /* frames per second */
+#define CD_SYNC_SIZE         12 /* 12 sync bytes per raw data frame */
+#define CD_MSF_OFFSET       150 /* MSF numbering offset of first frame */
+#define CD_CHUNK_SIZE        24 /* lowest-level "data bytes piece" */
+#define CD_NUM_OF_CHUNKS     98 /* chunks per frame */
+#define CD_FRAMESIZE_SUB     96 /* subchannel data "frame" size */
+#define CD_HEAD_SIZE          4 /* header (address) bytes per raw data frame */
+#define CD_SUBHEAD_SIZE       8 /* subheader bytes per raw XA data frame */
+#define CD_EDC_SIZE           4 /* bytes EDC per most raw data frame types */
+#define CD_ZERO_SIZE          8 /* bytes zero per yellow book mode 1 frame */
+#define CD_ECC_SIZE         276 /* bytes ECC per most raw data frame types */
+#define CD_FRAMESIZE       2048 /* bytes per frame, "cooked" mode */
+#define CD_FRAMESIZE_RAW   2352 /* bytes per frame, "raw" mode */
+#define CD_FRAMESIZE_RAWER 2646 /* The maximum possible returned bytes */ 
+/* most drives don't deliver everything: */
+#define CD_FRAMESIZE_RAW1 (CD_FRAMESIZE_RAW-CD_SYNC_SIZE) /*2340*/
+#define CD_FRAMESIZE_RAW0 (CD_FRAMESIZE_RAW-CD_SYNC_SIZE-CD_HEAD_SIZE) /*2336*/
+
+#define CD_XA_HEAD        (CD_HEAD_SIZE+CD_SUBHEAD_SIZE) /* "before data" part of raw XA frame */
+#define CD_XA_TAIL        (CD_EDC_SIZE+CD_ECC_SIZE) /* "after data" part of raw XA frame */
+#define CD_XA_SYNC_HEAD   (CD_SYNC_SIZE+CD_XA_HEAD) /* sync bytes + header of XA frame */
+
+/* CD-ROM address types (cdrom_tocentry.cdte_libdiscmage) */
+#define	CDROM_LBA 0x01 /* "logical block": first frame is #0 */
+#define	CDROM_MSF 0x02 /* "minute-second-frame": binary, not bcd here! */
+
+/* bit to tell whether track is data or audio (cdrom_tocentry.cdte_ctrl) */
+#define	CDROM_DATA_TRACK	0x04
+
+/* The leadout track is always 0xAA, regardless of # of tracks on disc */
+#define	CDROM_LEADOUT		0xAA
+#endif // <linux/cdrom.h>
+
+#if 0
+typedef struct
+  {
+    char *extension;
+    char *magic;                // magic bytes in file?
+    int32_t (*play) (const char *, int32_t, int32_t);
+  } libdiscmage_t;
+  
+extern const libdiscmage_t libdiscmage[];
+#endif
+
+#define MODE1_2048 0
+#define MODE1_2352 1
+#define MODE2_2336 2
+#define MODE2_2352 3
+// Macintosh
+#define MODE2_2056 4
+
+#define DEFAULT_FORMAT   0
+#define ISO_FORMAT       1
+#define BIN_FORMAT       2
+
+
+#define READ_BUF_SIZE  (1024*1024)
+#define WRITE_BUF_SIZE (1024*1024)
+
+
+typedef struct
+{
+/*
+  image nfo
+*/
+  uint32_t header_offset;
+  uint32_t header_position;
+  uint32_t image_length;
+  uint32_t version;
+  unsigned short sessions;      // # of sessions
+  unsigned short tracks;        // # of tracks
+  unsigned short remaining_sessions;    // sessions left
+  unsigned short remaining_tracks;      // tracks left
+  unsigned short global_current_session;        // current session
+/*
+  track nfo
+  TODO make an array of this
+*/
+  unsigned short global_current_track;  // current track
+  unsigned short number;
+  uint32_t position;
+  uint32_t mode;
+  uint32_t sector_size;
+  uint32_t sector_size_value;
+  uint32_t track_length;
+  uint32_t pregap_length;
+  uint32_t total_length;
+  uint32_t start_lba;
+  uint32_t filename_length;
+/*
+  workflow
+  TODO make this dissappear
+*/
+  int32_t track_type,save_as_iso,pregap,convert, fulldata, cutall, cutfirst;
+  char do_convert, do_cut;
+}
+st_image_t;
+
+extern void lba2msf (uint32_t lba, struct cdrom_msf *msf);
+extern int32_t bcd_conv (int32_t value);
+extern int32_t frame2bcd (int32_t frame);
+extern void frame2msf (int32_t frame, struct cdrom_msf *msf);
+extern int32_t msf2frame (struct cdrom_msf *msf);
+extern int32_t msf2lba (struct cdrom_msf *msf);
+              
+
+extern int32_t trackmode_probe (const char *image);
+
+extern int32_t bin2iso (const char *image, int32_t track_mode);
+extern int32_t cdirip (const char *image);
+//extern int32_t nerorip (const char *image);
+extern int32_t cdi2nero (const char *image);
+//extern int32_t isofix (const char *image);
+
+typedef struct
+{
+  uint32_t mode;
+  uint32_t sector_size;
+  uint32_t seek_header;
+  uint32_t seek_ecc;
+  char *common;
+  char *cdrdao;
+}
+st_track_modes_t;
+
+extern const st_track_modes_t track_modes[];
+
+#include "cdi.h"
+#include "bin.h"
+#include "iso.h"
+#include "nero.h"
+
+
+#endif  // LIBDISCMAGE_H
---
+++ trunk/ucon64/src/libdiscmage/misc.h
@@ -0,0 +1,395 @@
+/*
+misc.h - miscellaneous functions
+
+written by 1999 - 2002 NoisyB (noisyb@gmx.net)
+           2001 - 2002 dbjh
+                  2002 Jan-Erik Karlsson (Amiga)
+           
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+#ifndef MISC_H
+#define MISC_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __sun
+#ifdef __SVR4
+#define __solaris__
+#endif
+#endif
+
+#include <limits.h>
+#include <time.h>                               // gauge() prototype contains time_t
+#include <stdio.h>
+#include <dirent.h>
+#include "config.h"                             // ZLIB, ANSI_COLOR support
+
+#ifdef  ZLIB
+#include <zlib.h>
+#include "unzip.h"
+
+extern FILE *fopen2 (const char *filename, const char *mode);
+extern int fclose2 (FILE *file);
+extern int fseek2 (FILE *file, long offset, int mode);
+extern size_t fread2 (void *buffer, size_t size, size_t number, FILE *file);
+extern int fgetc2 (FILE *file);
+extern char *fgets2 (char *buffer, int maxlength, FILE *file);
+extern int feof2 (FILE *file);
+extern size_t fwrite2 (const void *buffer, size_t size, size_t number, FILE *file);
+extern int fputc2 (int character, FILE *file);
+
+#undef feof                                     // necessary on (at least) Cygwin
+
+#define fopen   fopen2
+#define fclose  fclose2
+#define fseek   fseek2
+#define fread   fread2
+#define fgetc   fgetc2
+#define fgets   fgets2
+#define feof    feof2
+#define fwrite  fwrite2
+#define fputc   fputc2
+
+#endif
+
+#ifndef  __CYGWIN__
+#include <inttypes.h>                           // int32_t and uint32_t
+#else
+#include <sys/types.h>
+typedef u_int8_t uint8_t;
+typedef u_int16_t uint16_t;
+typedef u_int32_t uint32_t;
+typedef u_int64_t uint64_t;
+#endif
+
+
+#ifndef FALSE
+#define FALSE 0
+#endif
+
+#ifndef TRUE
+#define TRUE 1
+#endif
+
+#ifndef MAXBUFSIZE
+#define MAXBUFSIZE 32768
+#endif // MAXBUFSIZE
+
+#ifndef ARGS_MAX
+#define ARGS_MAX 128
+#endif // ARGS_MAX
+
+#ifndef MIN
+#define MIN(a,b) ((a)<(b)?(a):(b))
+#endif
+#ifndef MAX
+#define MAX(a,b) ((a)>(b)?(a):(b))
+#endif
+
+#define NULL_TO_EMPTY(str) ((str) ? (str) : (""))
+
+//#define RANDOM(min, max) ((rand () % (max - min)) + min)
+
+#define OFFSET(a, offset) ((((unsigned char *)&(a))+(offset))[0])
+
+#ifdef WORDS_BIGENDIAN
+#undef WORDS_BIGENDIAN
+#endif
+
+#if     defined _LIBC || defined __GLIBC__
+  #include <endian.h>
+  #if __BYTE_ORDER == __BIG_ENDIAN
+    #define WORDS_BIGENDIAN 1
+  #endif
+#elif   defined AMIGA || defined __sparc__ || defined __BIG_ENDIAN__ || defined __APPLE__
+  #define WORDS_BIGENDIAN 1
+#endif
+
+#ifdef  __MSDOS__                               // __MSDOS__ must come before __unix__,
+  #define CURRENT_OS_S "MSDOS"                  //  because DJGPP defines both
+#elif   defined __unix__
+  #ifdef  __CYGWIN__
+    #define CURRENT_OS_S "Win32"
+  #elif   defined __FreeBSD__
+    #define CURRENT_OS_S "Unix (FreeBSD)"
+  #elif   defined __linux__
+    #define CURRENT_OS_S "Unix (Linux)"
+  #elif   defined __solaris__
+    #ifdef __sparc__
+      #define CURRENT_OS_S "Unix (Solaris/Sparc)"
+    #else
+      #define CURRENT_OS_S "Unix (Solaris/i386)"
+    #endif
+  #else
+    #define CURRENT_OS_S "Unix"
+  #endif
+#elif   defined __APPLE__
+  #if   defined __POWERPC__ || defined __ppc__
+    #define CURRENT_OS_S "Apple (ppc)"
+  #else
+    #define CURRENT_OS_S "Apple"
+  #endif
+#elif   defined __BEOS__
+  #define CURRENT_OS_S "BeOS"
+#elif defined AMIGA
+  #if defined __PPC__
+    #define CURRENT_OS_S "Amiga (ppc)"
+  #else
+    #define CURRENT_OS_S "Amiga (68k)"
+  #endif
+#else
+  #define CURRENT_OS_S "?"
+#endif
+
+
+#ifdef WORDS_BIGENDIAN
+#define me2be_16(x) (x)
+#define me2be_32(x) (x)
+#define me2be_64(x) (x)
+#define me2be_n(x,n) 
+#define me2le_16(x) (bswap_16(x))
+#define me2le_32(x) (bswap_32(x))
+#define me2le_64(x) (bswap_64(x))
+#define me2le_n(x,n) (mem_swap(x,n))
+#else
+#define me2be_16(x) (bswap_16(x))
+#define me2be_32(x) (bswap_32(x))
+#define me2be_64(x) (bswap_64(x))
+#define me2be_n(x,n) (mem_swap(x,n))
+#define me2le_16(x) (x)
+#define me2le_32(x) (x)
+#define me2le_64(x) (x)
+#define me2le_n(x,n) 
+#endif
+
+
+#ifdef __MSDOS__
+#define FILE_SEPARATOR '\\'
+#define FILE_SEPARATOR_S "\\"
+#else
+#define FILE_SEPARATOR '/'
+#define FILE_SEPARATOR_S "/"
+#endif
+
+#define OPTION '-'
+#define OPTION_S " -"
+#define OPTION_LONG_S "--"
+
+
+#if (defined __unix__ || defined __BEOS__ || defined AMIGA)
+#ifndef __MSDOS__
+#define getch           getchar                 // getchar() acts like DOS getch() after init_conio()
+extern int kbhit (void);                        // may only be used after init_conio()!
+#else
+#include <conio.h>                              // getch()
+#include <pc.h>                                 // kbhit()
+#endif
+// Should we leave these prototypes visible to the DOS code? The advantage
+//  would be a few less #ifdef's
+extern void init_conio (void);
+extern void deinit_conio (void);
+#endif
+
+
+/*
+  String manipulation
+
+  areprint() like isprint() but for a whole string
+  areupper() like isupper() but for a whole string
+  strupr()   convert string to upper-case
+  strlwr()   convert string to lower-case
+  mkprint()  convert all chars to isprint()'s
+  mkfile()   convert string into a correct file name
+  strtrim()  trim isspace()'s from start and end of string
+
+  setext() set/replace extension of filename with ext
+  getext() get extension of filename
+    extension means in this case the extension INCLUDING the dot '.'
+
+  basename2() GNU basename() clone
+*/
+extern int areupper (const char *str);
+extern int areprint (const char *str, int size);
+extern char *strupr (char *str);
+extern char *strlwr (char *str);
+extern char *strtrim (char *str);
+extern char *mkprint (char *str, const unsigned char replacement);
+extern char *mkfile (char *str, const unsigned char replacement);
+#define stricmp strcasecmp
+#define strnicmp strncasecmp
+extern char *setext (char *filename, const char *ext);
+extern const char *getext (const char *filename);
+#define EXTCMP(filename, ext) (strcasecmp (getext (filename), ext))
+extern char *basename2 (const char *str);
+
+
+/*
+  mem functions
+
+  memwcmp()    memcmp with wildcard support
+  mem_swap()    swap n Bytes from add on
+  mem_hexdump() hexdump n Bytes from add on; you can use here a virtual_start for the displayed counter
+  mem_crc16()  calculate the crc16 of buffer for size bytes
+  mem_crc32()  calculate the crc32 of buffer for size bytes
+*/
+extern int memwcmp (const void *add, const void *add_with_wildcards, size_t n, int wildcard);
+extern void mem_hexdump (const void *add, size_t n, int virtual_start);
+extern unsigned short mem_crc16 (unsigned int size, unsigned short crc16, const void *buffer);
+#ifdef  ZLIB
+#define mem_crc32(SIZE, CRC, BUF)       crc32(CRC, BUF, SIZE)
+#else
+extern unsigned int mem_crc32 (unsigned int size, unsigned int crc32, const void *buffer);
+#endif
+extern void *mem_swap (void *add, size_t n);
+#if 0
+#define bswap_16(x) ((unsigned short int)mem_swap(x,2,2))
+#define bswap_32(x) ((unsigned int)mem_swap(x,4,4))
+#define bswap_64(x) ((unsigned long long int)mem_swap(x,8,8))
+#else
+extern unsigned short int bswap_16 (unsigned short int x);
+extern unsigned int bswap_32 (unsigned int x);
+extern unsigned long long int bswap_64 (unsigned long long int x);
+#endif
+
+
+/*
+  Misc stuff
+
+  change_string() see header of implementation for usage
+  ansi_init ()    initalize ansi output
+  ansi_strip ()   strip ansi codes from a string
+  gauge()         init_time == time when gauge() was first started or when
+                  the transfer did start
+                  pos == current position
+                  size == full size
+                  gauge given these three values will calculate many
+                  informative things like time, status bar, cps, etc.
+                  it can be used for procedures which take some time to
+                  inform the user about the actual progress
+  getenv2()       getenv() clone for enviroments w/o HOME, TMP or TEMP variables
+  rmdir2()        like rmdir but removes non-empty directories recursively
+  tmpnam2()       replacement for tmpnam() temp must have the size of FILENAME_MAX
+  tmpnam3()       like tmpnam2() but creates file or dir at the same time to
+                  prevent double usage
+  renlwr()        rename all files in dir to lowercase
+  drop_privileges() switch to the real user and group id (leave "root mode")
+  register_func() atexit() replacement
+                  returns -1 if it fails, 0 if it was successful
+  unregister_func() unregisters a previously registered function
+                  returns -1 if it fails, 0 if it was successful
+  handle_registered_funcs() calls all the registered functions
+  wait2           wait (sleep) a specified number of milliseconds
+*/
+extern void change_string (char *searchstr, int strsize, char wc, char esc,
+                           char *end, int endsize, char *buf, int bufsize,
+                           int offset, ...);
+#ifdef  ANSI_COLOR
+extern int ansi_init (void);
+extern char *ansi_strip (char *str);
+#endif
+extern int gauge (time_t init_time, int pos, int size);
+extern char *getenv2 (const char *variable);
+//#define getenv getenv2
+extern char *tmpnam2 (char *temp);
+#define TYPE_FILE 0
+#define TYPE_DIR 1
+extern char *tmpnam3 (char *temp, int type);
+extern int rmdir2 (const char *path);
+extern int renlwr (const char *dir);
+#if     defined __unix__ && !defined __MSDOS__
+extern int drop_privileges (void);
+#endif
+extern int register_func (void (*func) (void));
+extern int unregister_func (void (*func) (void));
+extern void handle_registered_funcs (void);
+extern void wait2 (int nmillis);
+
+
+/*
+  Map functions
+
+  map_create()    create a new map (associative array)
+  map_copy()      copy map src to map dest
+                  dest must be a larger map than src
+  map_put()       put object in map under key
+                  Callers should always reset the passed map pointer with the
+                  one this function returns. This is necessary in case the map
+                  had to be resized.
+  map_get()       get object from map stored under key
+                  returns NULL if there is no object with key in map
+  map_del()       remove the object stored under key from map
+  map_dump()      display the current contents of map
+
+  The value MAP_FREE_KEY is reserved as a special key value. Don't use
+  that value.
+
+  NOTE: Currently the map functions are only used by the zlib wrapper
+        functions, so they are only available if ZLIB is defined.
+        Remove the #ifdef ZLIB in this file and in misc.c if other code should
+        use them.
+*/
+#ifdef  ZLIB
+
+#define MAP_FREE_KEY 0
+
+typedef struct st_map_element
+{
+  void *key;
+  void *object;
+} st_map_element_t;
+
+typedef struct st_map
+{
+  st_map_element_t *data;
+  int size;
+} st_map_t;
+
+
+extern st_map_t *map_create (int n_elements);
+extern void map_copy (st_map_t *dest, st_map_t *src);
+extern st_map_t *map_put (st_map_t *map, void *key, void *object);
+extern void *map_get (st_map_t *map, void *key);
+extern void map_del (st_map_t *map, void *key);
+extern void map_dump (st_map_t *map);
+#endif // ZLIB
+
+/*
+  Configfile handling
+
+  get_property()    get value of propname from filename or return value of env
+                   with name like propname or return def
+  set_property()    set propname with value in filename
+  DELETE_PROPERTY() like set_property but when value of propname is NULL the
+                   whole property will disappear from filename
+*/
+extern const char *get_property (const char *filename, const char *propname, char *value, const char *def);
+extern int set_property (const char *filename, const char *propname, const char *value);
+#define DELETE_PROPERTY(a, b) (set_property(a, b, NULL))
+
+
+#ifdef  ZLIB
+// Returns the number of files in the "central dir of this disk" or -1 if
+//  filename is not a ZIP file or an error occured.
+extern int unzip_get_number_entries (const char *filename);
+extern int unzip_goto_file (unzFile file, int file_index);
+extern int unzip_current_file_nr;
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif // #ifndef MISC_H

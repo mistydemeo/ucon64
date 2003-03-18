3137a187e96b671bcac30d0c134fde3c5b438d36
noisyb <noisyb@ddfabd57-4c8b-41e2-9536-ec85e047d1b2>
2003-03-18 11:01:43 +0000

    *** empty log message ***

--- trunk/ucon64/src/ucon64_opts.c
+++ trunk/ucon64/src/ucon64_opts.c
@@ -528,7 +528,7 @@ ucon64_e (void)
       return -1;
     }
 
-  sprintf (buf, "%s %s", buf2, ucon64.rom);
+  sprintf (buf, "%s \"%s\"", buf2, ucon64.rom);
 
   printf ("%s\n", buf);
   fflush (stdout);

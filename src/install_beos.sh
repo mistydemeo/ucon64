af0b08216446fc095304add93041d39914552cee
dbjh <dbjh@ddfabd57-4c8b-41e2-9536-ec85e047d1b2>
2003-05-21 14:21:12 +0000

    - improved BeOS install script a bit

--- src/install_beos.sh
+++ src/install_beos.sh
@@ -8,38 +8,41 @@ uCON64 will be installed in $HOME/config/bin.
 
 Do you want to continue?" "Cancel" "Install")
 if [ "$areply" ==  "Install" ]; then
-  cp ucon64 $HOME/config/bin
-  if [ ! -e $HOME/.ucon64 ]; then
-  mkdir $HOME/.ucon64
+  cp ucon64 "$HOME/config/bin"
+  if [ ! -e "$HOME/.ucon64" ]; then
+    mkdir "$HOME/.ucon64"
   fi
-  if [ ! -e $HOME/.ucon64/dat ]; then
-  mkdir $HOME/.ucon64/dat
+  if [ ! -e "$HOME/.ucon64/dat" ]; then
+    mkdir "$HOME/.ucon64/dat"
   fi
   if [ -f libdiscmage/discmage.so ]; then
-  cp libdiscmage/discmage.so $HOME/.ucon64
+    cp libdiscmage/discmage.so "$HOME/.ucon64"
   elif [ -f discmage.so ]; then
-  cp discmage.so $HOME/.ucon64
+    cp discmage.so "$HOME/.ucon64"
   fi
   # ask for ioport driver installation
-  drreply=$(alert "uCON64 needs the BeOS ioport driver by Caz Jones.
+  drreply=$(alert "In order to communicate with a backup device (\"copier\") uCON64 needs the BeOS ioport driver by Caz Jones. You needn't install it if you don't want to use uCON64 for communicating with a copier.
 
 Would you like to do install it?" "No" "Yes, install driver")
   if [ "$drreply" ==  "Yes, install driver" ]; then
     if [ -e "$HOME/ioport" ]; then
       cd "$HOME/ioport/driver"
       install_ioport
+    elif [ -e ioport.zip ]; then
+      unzip ioport.zip -d "$HOME"
+      cd "$HOME/ioport/driver"
+      install_ioport
+    elif [ -e "$HOME/ioport.zip" ]; then
+      unzip "$HOME/ioport.zip" -d "$HOME"
+      cd "$HOME/ioport/driver"
+      install_ioport
     else
-      if [ -e "$HOME/ioport.zip" ]; then
-        unzip $HOME/ioport.zip -d $HOME
-        cd "$HOME/ioport/driver"
-        install_ioport
-      else
-        alert "Please download the latest version of the ioport driver from either
+      alert "Please download the latest version of the ioport driver from either
 http://www.infernal.currantbun.com or
 http://ucon64.sourceforge.net
 and copy it to $HOME. Then retry to install the driver."
-        exit
-      fi
+      # exit
+      # Don't exit as uCON64 itself has been successfully installed
     fi
   fi
 

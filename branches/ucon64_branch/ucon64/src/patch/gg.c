765aaa114ac4849a0b6a9333d79de7bc3c163437
noisyb <noisyb@ddfabd57-4c8b-41e2-9536-ec85e047d1b2>
2003-06-06 20:46:11 +0000

    *** empty log message ***

--- branches/ucon64_branch/ucon64/src/patch/gg.c
+++ branches/ucon64_branch/ucon64/src/patch/gg.c
@@ -1,5 +1,5 @@
 /********************************************************************
- * $Id: gg.c,v 1.36.2.5 2003-06-06 13:33:58 noisyb Exp $
+ * $Id: gg.c,v 1.36.2.6 2003-06-06 20:46:11 noisyb Exp $
  *
  * Copyright (c) 2001 by WyrmCorp <http://wyrmcorp.com>.
  * All rights reserved. Distributed under the BSD Software License.

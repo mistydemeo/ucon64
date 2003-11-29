c444b86bdeda9eb83333043264a7cc0d6b6c71ce
noisyb <noisyb@ddfabd57-4c8b-41e2-9536-ec85e047d1b2>
2003-11-29 23:49:40 +0000

    nb: libnetgui

--- branches/ucon64_branch/ucon64/src/ucon64_defines.h
+++ branches/ucon64_branch/ucon64/src/ucon64_defines.h
@@ -34,7 +34,7 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 #define UCON64_UNKNOWN_S "Unknown"
 #define NULL_TO_UNKNOWN_S(str) ((str) ? (str) : (UCON64_UNKNOWN_S))
 
-#define UCON64_VERSION_S "1.9.8-3"
+#define UCON64_VERSION_S "1.9.9-1"
 
 /* program version counter */
 //#define UCON64_VERSION (198)

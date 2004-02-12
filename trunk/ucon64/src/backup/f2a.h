76727ce3d940fc2a195afee77b8073017b4cf2d0
dbjh <dbjh@ddfabd57-4c8b-41e2-9536-ec85e047d1b2>
2004-02-12 00:25:02 +0000

    *** empty log message ***

---
+++ trunk/ucon64/src/backup/f2a.h
@@ -0,0 +1,35 @@
+/*
+f2a.h - Flash 2 Advance support for uCON64
+
+written by 2003 Ulrich Hecht (uli@emulinks.de)
+           2004 NoisyB (noisyb@gmx.net)
+
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+#ifndef F2A_H
+#define F2A_H
+extern const st_usage_t f2a_usage[];
+
+#define F2A_HEADER_START 0
+#define F2A_HEADER_LEN 0
+
+#ifdef PARALLEL
+extern int f2a_read_rom (const char *filename, unsigned int parport, int size);
+extern int f2a_write_rom (const char *filename, unsigned int parport);
+extern int f2a_read_sram (const char *filename, unsigned int parport, int bank);
+extern int f2a_write_sram (const char *filename, unsigned int parport, int bank);
+#endif // PARALLEL
+#endif

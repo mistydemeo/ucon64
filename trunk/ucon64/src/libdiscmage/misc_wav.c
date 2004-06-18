9965ff5d14fb1d5527ac4dd899690cf0340a18b1
dbjh <dbjh@ddfabd57-4c8b-41e2-9536-ec85e047d1b2>
2004-06-18 21:12:44 +0000

    - forced commit to see if it removes executable attribute

--- trunk/ucon64/src/libdiscmage/misc_wav.c
+++ trunk/ucon64/src/libdiscmage/misc_wav.c
@@ -22,7 +22,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include "misc.h"
 
 void
-writewavheader (FILE * fh, int track_length)
+writewavheader (FILE *fh, int track_length)
 {
   typedef struct
     {

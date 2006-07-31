01e5c33ac8c3e2dea1553c2c51ab28ede65d2c85
noisyb <noisyb@ddfabd57-4c8b-41e2-9536-ec85e047d1b2>
2006-07-31 15:16:45 +0000

    *** empty log message ***

--- trunk/htdocs/template.sh
+++ trunk/htdocs/template.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-rsstool rss.slashdot.org/slashdot/slashdotgames --template=index.template -r >index.htm
+#rsstool "rss.slashdot.org/slashdot/slashdotgames" --template=index.template -r >index.htm
+rsstool "rss.slashdot.org/slashdot/slashdotgames" \
+ "http://news.google.com/news?hl=en&ned=us&q=wii,+nintendo,+playstation,+ps3,+xbox&ie=UTF-8&output=rss" \
+ --template=index.template -r >index.htm
 ln -sf index.htm index.html
 ln -sf index.htm index2.htm
 ln -sf index.htm index2.html

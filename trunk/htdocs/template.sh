23a6a3b03dab9437df56a8e84877d1411ffc418c
noisyb <noisyb@ddfabd57-4c8b-41e2-9536-ec85e047d1b2>
2006-07-31 12:27:51 +0000

    *** empty log message ***

--- trunk/htdocs/template.sh
+++ trunk/htdocs/template.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-rsstool rss.slashdot.org/slashdot/slashdotgames --template=index.template -r >/index.htm
+rsstool rss.slashdot.org/slashdot/slashdotgames --template=index.template -r >index.htm
+ln -sf index.htm index.html
+ln -sf index.htm index2.htm
+ln -sf index.htm index2.html

ff738de372ad4f8d12837c6f0f657b734a43ca60
noisyb <noisyb@ddfabd57-4c8b-41e2-9536-ec85e047d1b2>
2006-08-08 20:36:37 +0000

    *** empty log message ***

--- trunk/htdocs/template.sh
+++ trunk/htdocs/template.sh
@@ -1,14 +1,14 @@
-#!/bin/sh
-#http://www.youtube.com/rss/tag/snes.rss
-#rsstool "rss.slashdot.org/slashdot/slashdotgames" --template=index.template -r >index.htm
-#rsstool "rss.slashdot.org/slashdot/slashdotgames" \
-# "http://news.google.com/news?hl=en&ned=us&q=wii,+nintendo,+playstation,+ps3,+xbox&ie=UTF-8&output=rss" \
-# --template=index.template -r >index.htm
+#"http://rss.slashdot.org/slashdot/slashdotgames" \
+#"http://news.google.com/news?hl=en&ned=us&q=wii,+nintendo,+playstation,+ps3,+xbox&ie=UTF-8&output=rss" \
 rsstool --template=index.template -r \
 "http://www.youtube.com/rss/tag/nintendo+emulation.rss" \
 "http://www.youtube.com/rss/tag/gp2x+emulation.rss" \
 "http://www.youtube.com/rss/tag/sega+emulation.rss" \
 "http://www.youtube.com/rss/tag/sony+emulation.rss" \
+"http://www.youtube.com/rss/tag/play+station+emulation.rss" \
+"http://www.youtube.com/rss/tag/ps2+emulation.rss" \
+"http://www.youtube.com/rss/tag/dreamcast+emulation.rss" \
+"http://www.youtube.com/rss/tag/snes+emulation.rss" \
 >index.htm
 ln -sf index.htm index.html
 ln -sf index.htm index2.htm

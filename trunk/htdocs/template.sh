4c6755c112603bf4169892db709ddbe4739af265
noisyb <noisyb@ddfabd57-4c8b-41e2-9536-ec85e047d1b2>
2006-08-25 22:31:48 +0000

    *** empty log message ***

--- trunk/htdocs/template.sh
+++ trunk/htdocs/template.sh
@@ -1,15 +1,30 @@
-#"http://rss.slashdot.org/slashdot/slashdotgames" \
-#"http://news.google.com/news?hl=en&ned=us&q=wii,+nintendo,+playstation,+ps3,+xbox&ie=UTF-8&output=rss" \
+SEARCH1="nintendo+emulation"
+SEARCH2="gp2x+emulation"
+SEARCH3="sega+emulation"
+SEARCH4="sony+emulation"
+SEARCH5="play+station+emulation"
+SEARCH6="ps2+emulation"
+SEARCH7="dreamcast+emulation"
+SEARCH8="snes+emulation"
 rsstool --template=index.template -r \
-"http://www.youtube.com/rss/tag/nintendo+emulation.rss" \
-"http://www.youtube.com/rss/tag/gp2x+emulation.rss" \
-"http://www.youtube.com/rss/tag/sega+emulation.rss" \
-"http://www.youtube.com/rss/tag/sony+emulation.rss" \
-"http://www.youtube.com/rss/tag/play+station+emulation.rss" \
-"http://www.youtube.com/rss/tag/ps2+emulation.rss" \
-"http://www.youtube.com/rss/tag/dreamcast+emulation.rss" \
-"http://www.youtube.com/rss/tag/snes+emulation.rss" \
+"http://www.youtube.com/rss/tag/"$SEARCH1".rss" \
+"http://www.youtube.com/rss/tag/"$SEARCH2".rss" \
+"http://www.youtube.com/rss/tag/"$SEARCH3".rss" \
+"http://www.youtube.com/rss/tag/"$SEARCH4".rss" \
+"http://www.youtube.com/rss/tag/"$SEARCH5".rss" \
+"http://www.youtube.com/rss/tag/"$SEARCH6".rss" \
+"http://www.youtube.com/rss/tag/"$SEARCH7".rss" \
+"http://www.youtube.com/rss/tag/"$SEARCH8".rss" \
+"http://video.google.com/videosearch?q="$SEARCH1"+is%3Afree&page=1&so=1&output=rss" \
+"http://video.google.com/videosearch?q="$SEARCH2"+is%3Afree&page=1&so=1&output=rss" \
+"http://video.google.com/videosearch?q="$SEARCH3"+is%3Afree&page=1&so=1&output=rss" \
+"http://video.google.com/videosearch?q="$SEARCH4"+is%3Afree&page=1&so=1&output=rss" \
+"http://video.google.com/videosearch?q="$SEARCH5"+is%3Afree&page=1&so=1&output=rss" \
+"http://video.google.com/videosearch?q="$SEARCH6"+is%3Afree&page=1&so=1&output=rss" \
+"http://video.google.com/videosearch?q="$SEARCH7"+is%3Afree&page=1&so=1&output=rss" \
+"http://video.google.com/videosearch?q="$SEARCH8"+is%3Afree&page=1&so=1&output=rss" \
 >index.htm
 ln -sf index.htm index.html
 ln -sf index.htm index2.htm
 ln -sf index.htm index2.html
+scp index.htm index.html index2.htm index2.html noisyb@ucon64.sf.net:/home/groups/u/uc/ucon64/htdocs

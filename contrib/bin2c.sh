3ec82e481507085eb35d9672758495909d59f934
noisyb <noisyb@ddfabd57-4c8b-41e2-9536-ec85e047d1b2>
2007-01-01 17:59:45 +0000

    *** empty log message ***

--- contrib/bin2c.sh
+++ contrib/bin2c.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-echo "const unsigned char $1[] = {"
-hexdump -v -e '"  " 8/1 "0x%02x, " "\n"' $1
+echo "const unsigned char `echo $1|sed -e 's:\.:_:g' -e 'y:ABCDEFGHIJKLMNOPQRSTUVWXYZ:abcdefghijklmnopqrstuvwxyz:'`[`du -b $1|sed -e "s:$1::"`] = {"
+cat $1|hexdump -v -e '"  " 8/1 "0x%02x, " "\n"'|sed -e 's: 0x  ,::g'
 echo "};"

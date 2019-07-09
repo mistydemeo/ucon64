f978c92cf6e9a5a94801117b56735fe78f20281c
dbjh <dbjh@ddfabd57-4c8b-41e2-9536-ec85e047d1b2>
2019-07-09 00:11:20 +0000

    - Fixed old mistake in Makefile of libcd64 for NetBSD and OpenBSD when not a   default build. - Solved issue with unclear status and portability of inb() & outb() on NetBSD   by making all x86 platforms (except BeOS) use inline assembly, proving me   right 15 years ago when I wrote "using a bit of inline assembly is not such a   bad idea at all". - Replaced bashism in configure{.in}. - fgets() now appears to be redefined by default on NetBSD, causing many   warnings. So, it's now unconditonally undefined and redefined in archive.h   and misc_z.h.

--- branches/ucon64-2.2.0/src/libdiscmage/misc_z.h
+++ branches/ucon64-2.2.0/src/libdiscmage/misc_z.h
@@ -1,7 +1,7 @@
 /*
 misc_z.h - miscellaneous zlib functions
 
-Copyright (c) 2001 - 2004, 2016 - 2017 dbjh
+Copyright (c) 2001 - 2004, 2016 - 2017, 2019 dbjh
 
 
 This program is free software; you can redistribute it and/or modify
@@ -58,6 +58,7 @@ extern off_t q_fsize2 (const char *filename);
 #define fseek(FILE, OFFSET, MODE) fseek2(FILE, OFFSET, MODE)
 #define fread(BUF, SIZE, NUM, FILE) fread2(BUF, SIZE, NUM, FILE)
 #define fgetc(FILE) fgetc2(FILE)
+#undef  fgets                                   // necessary on (at least) NetBSD
 #define fgets(BUF, MAXLEN, FILE) fgets2(BUF, MAXLEN, FILE)
 #undef  feof                                    // necessary on (at least) Cygwin
 #define feof(FILE) feof2(FILE)
